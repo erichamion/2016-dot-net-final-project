b84e2f7fad282fafa2ee1d670602ccae117c942d
Eric Ray <erichamion@gmail.com>
2016-09-26 19:41:11 -0500

    Added date selection to front end

--- Performance/Performance/Scripts/Local/site.js
+++ Performance/Performance/Scripts/Local/site.js
@@ -1,8 +1,26 @@
-﻿var mainApp = angular.module("performanceApp", [])
+﻿function DateToYMD(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var twoDigitMonth = ("00" + month).slice(-2);
+    var day = date.getDate();
+    var twoDigitDay = ("00" + day).slice(-2);
+    return year + "-" + twoDigitMonth + "-" + twoDigitDay;
+}
+
+var mainApp = angular.module("performanceApp", [])
     .controller("statController", function ($scope, $http) {
         $scope.update = function () {
-            var url = baseUrl +  $scope.employeeId;
-            $scope.header = $scope.employeeId ? $scope.employeeId + ": Today" : "Please choose an employee";
+            var url = baseUrl + $scope.employeeId;
+            $scope.startDateDescription = "Today";
+            $scope.endDateDescription = "Today";
+            if ($scope.startDate) {
+                $scope.startDateDescription = DateToYMD($scope.startDate);
+                url += "/" + $scope.startDateDescription;
+                if ($scope.endDate) {
+                    $scope.endDateDescription = DateToYMD($scope.endDate);
+                    url += "/" + $scope.endDateDescription;
+                }
+            }
             $http.get(url)
                 .success(function (resp) {
                     $scope.stats = resp;
